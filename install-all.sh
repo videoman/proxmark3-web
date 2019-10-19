078901ec17f5077bbb402b25b85ff484c72d60b1
David Bryan <dmbryan@us.ibm.com>
2019-10-18 22:00:27 -0500

    updated binary name

--- install-all.sh
+++ install-all.sh
@@ -3,7 +3,8 @@
 # this script will install proxmark3-rdv4, and all the things.
 
 sudo apt-get update && sudo apt-get -y install --no-install-recommends git ca-certificates build-essential pkg-config \
-libreadline-dev gcc-arm-none-eabi libnewlib-dev python-flask && cd ~ && git clone https://github.com/RfidResearchGroup/proxmark3.git &&\
+libreadline-dev gcc-arm-none-eabi libnewlib-dev python3-flask && \\
+cd ~ && git clone https://github.com/RfidResearchGroup/proxmark3.git &&\
 cd ~/proxmark3/ && git pull && make clean && make all && sudo make install &&\
 cd ~/proxmark3-web && \
 ./install.sh
