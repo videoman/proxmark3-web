0af4a1216b958c72fb8d5a885c4bfadc4a0dc701
David Bryan <dmbryan@us.ibm.com>
2019-10-18 22:37:37 -0500

    updates

--- install-all.sh
+++ install-all.sh
@@ -3,7 +3,7 @@
 # this script will install proxmark3-rdv4, and all the things.
 
 sudo apt-get update && sudo apt-get -y install --no-install-recommends git ca-certificates build-essential pkg-config \
-libreadline-dev gcc-arm-none-eabi libnewlib-dev python3-flask && \\
+libreadline-dev gcc-arm-none-eabi libnewlib-dev python3-flask hostapd && \\
 cd ~ && git clone https://github.com/RfidResearchGroup/proxmark3.git &&\
 cd ~/proxmark3/ && git pull && make clean && make all && sudo make install &&\
 cd ~/proxmark3-web && \
